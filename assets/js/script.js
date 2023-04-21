ef8c8bdd499f592f9bd0f9d0ed3ca20331dca671
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-21 19:25:03 +0100

    Fixing general bugs with the highscore system

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,62 +15,35 @@ const nameInputEL = document.querySelector(`input`);
 const errorMessageEl = document.querySelector(`.error-message`);
 const highscoreTitleEl = document.querySelector(`.highscore-container`);
 
+//Setting general variables
 let timer;
 let time = 0;
 let currentQuestion = 0;
 let score = 0;
 
-//Start Game
-startBtn.addEventListener(`click`, function (e) {
-  time = 75;
-  timerEl.textContent = setTimerVisuals();
-  showSection(welcomeSect, false);
-  questionSect.classList.remove(`hidden`);
-
-  addQuestions();
-  timer = setInterval(startTimer, 1000);
-});
-
-//Timer functions
+//Timer function, start a timer when it hits 0, the game ends
 function startTimer() {
   time--;
   timerEl.textContent = setTimerVisuals();
 
-  if (time === 0) {
+  if (time <= 0) {
     clearInterval(timer);
     endGame();
   }
 }
 
+//Sets the visuals for the timer to 00:00 format
 function setTimerVisuals() {
-  const minutes = Math.floor(time / 60);
+  const minutes = String(Math.floor(time / 60)).padStart(2, 0);
   const seconds = String(time % 60).padStart(2, 0);
 
   return `${minutes}:${seconds}`;
 }
-
+/*When the game ends, takes the user to the highscore
+submitting section*/
 function endGame() {
   highscoreScreen();
-  /**Take the user to a new screen and ends the game */
 }
-
-//Question functions
-choiceEl.addEventListener(`click`, function (e) {
-  if (!e.target.matches(`.button`)) return;
-  console.log(e.target);
-  questionAns = questions[currentQuestion].answer;
-  if (e.target.textContent === questionAns) {
-    console.log(`Question is correct`);
-    answerCorrectly(true);
-  } else {
-    console.log(`Question is wrong`);
-    answerCorrectly(false);
-    time -= 15;
-  }
-
-  currentQuestion++;
-  addQuestions();
-});
 
 /**Adds the questions to the screen */
 function addQuestions() {
@@ -102,6 +75,7 @@ function answerCorrectly(gotCorrect) {
   }, 1000);
 }
 
+//Shows the submit section and asks the user to put in there initials
 function highscoreSubmitScreen() {
   /**Shows the next screen */
   clearInterval(timer);
@@ -119,6 +93,8 @@ function showSection(sect, shouldShow = true) {
 
 //Sorts the score by desending order and then displays to the screen
 function highscoreSection() {
+  currentQuestion = 0;
+  highscoreTitleEl.innerHTML = ``;
   const orderedScores = [];
   //Order all the scores
   Object.keys(localStorage).forEach(function (key) {
@@ -143,13 +119,45 @@ function highscoreSection() {
     `;
     highscoreTitleEl.insertAdjacentHTML(`beforeend`, scoresHTML);
   }
+  questionSect.classList.add(`hidden`);
   showSection(welcomeSect, false);
   showSection(highscoreSubmitEl, false);
   showSection(highscoreEl, true);
 }
 
-/**Submit button Handler */
+/**Event Handlers */
 
+//Start Game
+startBtn.addEventListener(`click`, function (e) {
+  time = 10;
+  timerEl.textContent = setTimerVisuals();
+  showSection(welcomeSect, false);
+  showSection(highscoreEl, false);
+  questionSect.classList.remove(`hidden`);
+
+  addQuestions();
+  timer = setInterval(startTimer, 1000);
+});
+
+//Question Handler
+choiceEl.addEventListener(`click`, function (e) {
+  if (!e.target.matches(`.button`)) return;
+  console.log(e.target);
+  questionAns = questions[currentQuestion].answer;
+  if (e.target.textContent === questionAns) {
+    console.log(`Question is correct`);
+    answerCorrectly(true);
+  } else {
+    console.log(`Question is wrong`);
+    answerCorrectly(false);
+    time -= 15;
+  }
+
+  currentQuestion++;
+  addQuestions();
+});
+
+//Submit name and score handler
 submitBtn.addEventListener(`click`, function () {
   const name = nameInputEL.value;
   console.log(name);
@@ -162,11 +170,13 @@ submitBtn.addEventListener(`click`, function () {
   }
 });
 
+//Showing highscore
 highscoreBtn.addEventListener(`click`, function () {
-  highscoreTitleEl.innerHTML = ``;
+  clearInterval(timer);
   highscoreSection();
 });
 
+//Return back from highscore
 returnBtn.addEventListener(`click`, function () {
   showSection(highscoreEl, false);
   showSection(welcomeSect, true);
